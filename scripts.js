0b9d880915c607f932ba37e6329b85625db8bffa
Sofia <Sofia@db-kr.rbcgrp.com>
2021-05-01 11:44:18 +0300

    Ukraine forewer

--- scripts.js
+++ scripts.js
@@ -1,13 +1,19 @@
-let score=0;
+let score = 0;
+let pairsPerLevel = 2;
+let gameFinished = false;
+
 const rows = 8;
 const cells = 8;
-const items= rows*cells;
+const items = rows * cells;
 
 //main game field element
 const gameElement = document.getElementById('game');
 const scoreElement = document.getElementById('score');
 
-let icons=['fa-cannabis',
+let cards = [];
+let activeCards = [];
+
+ let icons=['fa-cannabis',
 'fa-certificate',
 'fa-canadian-maple-leaf',
 'fa-cloud-sun',
@@ -43,71 +49,154 @@ let icons=['fa-cannabis',
 'fas fa-puzzle-piece',
 'fad fa-chess-pawn',
 ];
+let letAvailableIcons = [];
+
+let openedPairCard = '';
+let closedCards = 0;
+
+function increaseScore() {
+  score++;
+  closedCards--;  
+  scoreElement.innerText = 'SCORE: ' + score;
+
+  if (closedCards == 0){
+    pairsPerLevel++;
+    generateLevel();
+  }
+}
+
+function generateLevel() {
+  openedPairCard = '';
+  closedCards = 0;
+  cards = [];
+  activeCards = [];
+  for (let i = 0; i < icons.length; i++) {
+    letAvailableIcons[i] = icons[i];
+  }
+
+  refreshGrid();
+  assignClicks();
+
+  //select random buttons
+  // chooseRandomCards(5);
+  chooseRandomPairCards(pairsPerLevel);
+
+  // Show & hide them
+  showActiveCards();
+  setTimeout(hideActiveCards, 2 * 1000);
+}
+
+function refreshGrid() {
+  //clear game
+  gameElement.innerHTML = '';
+  // dynamically create rows with cells
+  let itemCounter = 0;
+  for (let i = 0; i < items; i++) {
+    const newCell = document.createElement('div');
+    newCell.className = 'card';
+    newCell.innerText = itemCounter++;
+    gameElement.appendChild(newCell);
+  }
+}
+
+function assignClicks() {
+  let blocks = document.getElementsByClassName('card');
+  for (let i = 0; i < blocks.length; i++) {
+    const element = blocks[i];
+    // assign element to array
+    cards[i] = element;
+
+    element.addEventListener('click', () => clickHandler(i));
+  }
+}
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
+
+function chooseRandomPairCards(count) {
+  let availableItems = [];
+  let availableItemsCount = 0;
+  for (let i = 0; i < items; i++)
+    if (!activeCards[i]) availableItems[availableItemsCount++] = i;
+
+  for (let i = 0; i < count; i++) {
+    let icon = getRandomIcon();
+    if (icon == null) return;
+    for (let j = 0; j < 2; j++) {
+      let newRandom = getRandomInt(availableItemsCount);
+      let newIndex = availableItems[newRandom];
+      activeCards[newIndex] = icon;
+      availableItems.splice(newRandom, 1);
+      availableItemsCount--;
+      closedCards++;
+
+      cards[newIndex].classList.add('active');
+    }
+  }
+}
+
+function getRandomIcon() {
+  if (letAvailableIcons.length == 0) return null;
+  let iconIndex = getRandomInt(letAvailableIcons.length);
+  let icon = letAvailableIcons[iconIndex];
+  letAvailableIcons.splice(iconIndex, 1);
+  return icon;
+}
+
+function clickHandler(i) {
+  element = cards[i];
+
+  if (gameFinished || !activeCards[i] || element.classList.contains('flipped')) return;
+
+  let childIcon = element.querySelector('i');
+  if (openedPairCard != '') {
+    if (childIcon.classList.value == openedPairCard) {
+      openedPairCard = '';
+    } else {
+      finishGame();
+      return;
+    }
+  } else {
+    openedPairCard = childIcon.classList.value;
+  }
+  element.classList.toggle('flipped');
+  increaseScore();
+}
 
 function showActiveCards() {
-    for (let i = 0; i < items; i++) {
-      if (activeCards[i]) {
-        let element = cards[i];
-        element.classList.add('flipped');
-      }
+  for (let i = 0; i < items; i++) {
+    let icon = activeCards[i];
+    if (icon) {
+      let element = cards[i];
+      element.innerHTML = `<i class="fa ${icon}"></i>`;
+      element.classList.add('flipped');
     }
   }
-  function refreshGrid() {
-    //clear game
-    gameElement.innerHTML = '';
-    // dynamically create rows with cells
-    let itemCounter = 0;
-    for (let i = 0; i < items; i++) {
-      const newCell = document.createElement('div');
-      newCell.className = 'card active';
-      newCell.innerText = itemCounter++;
-      gameElement.appendChild(newCell);
+}
+
+function hideActiveCards() {
+  for (let i = 0; i < items; i++) {
+    if (activeCards[i]) {
+      let element = cards[i];
+      element.classList.remove('flipped');
     }
   }
-  function hideActiveCards() {
-    for (let i = 0; i < items; i++) {
-      if (activeCards[i]) {
-        let element = cards[i];
-        element.classList.remove('flipped');
-      }
-    }
+}
+
+function finishGame() {
+  gameFinished = true;
+  let maxScore = localStorage.getItem('max-score');
+  if (maxScore == null)
+    maxScore = 0;
+  
+  let message = `Reached: ${score}. Highest: ${maxScore}`;
+    if (score > maxScore) {
+    localStorage.setItem('max-score', score);
+    message = `WOW! New best score: ${score}`;
   }
-  function increaseScore() {
-    score++;
-    scoreElement.innerText = 'SCORE: ' + score;
-  }
-  function assignClicks() {
-    let blocks = document.getElementsByClassName('card');
-    for (let i = 0; i < blocks.length; i++) {
-      const element = blocks[i];
-      cards[i] = element;
-      element.addEventListener('click', () => clickHandler(i));
-    }
-  }
-  function chooseRandomCards(count) {
-    for (let i = 0; i < count; i++) {
-      let newIndex = getRandomInt(items);
-      activeCards[newIndex] = true;
-    }
-  }
-  function generateLevel () {
-    cards = [];
-    activeCards = [];
-    refreshGrid();
-    assignClicks();
-    //select random buttons
-    chooseRandomCards(5);
-    // Show & hide them
-    showActiveCards();
-    setTimeout(hideActiveCards, 7 * 1000);
-  }
-  function clickHandler(i) {
-    element = cards[i];
-    if (!activeCards[i]) return;
-    element.classList.toggle('flipped');
-    increaseScore();
-  }
-  function getRandomInt(max) {
-    return Math.floor(Math.random() * max);
-  }
+  scoreElement.innerText = message;
+  gameElement.classList.add('finished');
+}
+
 generateLevel();
